f7ad88d091fa865a89c8c4143975ebaddeb35c6b
Trey Carpenter <trey.carpenter@method.com>
2025-01-10 07:43:26 -0500

    Initial Commit

---
+++ Skeleton/Skeleton/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skeleton
+//
+//  Created by Trey Carpenter Iii on 1/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skeleton/Skeleton/SkeletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkeletonApp.swift
+//  Skeleton
+//
+//  Created by Trey Carpenter Iii on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SkeletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
